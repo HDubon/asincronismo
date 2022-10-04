e7b29fff594c201fb60b68d0e1928082dc1e09a4
HDubon <hidcoding@gmail.com>
2022-10-04 14:38:35 -0600

    tercer commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ function sum(num1, num2) {
     return num1 + num2;
   }
   
-  function calc(num1, num2, sumNumbers) {
-    return sumNumbers(num1, num2);
+  function calc(num1, num2, callback) {
+    return callback(num1, num2);
   };
   
   console.log(calc(2, 2, sum));
-  
+
+
+  // setTimeout contiene  en si mismo un callback al llamar una funcion anonima de temporizado
   setTimeout(function () {
     console.log('Hola JavaScript');
   }, 5000)
   
+
+  // otra forma de usar setTimeout 
   function gretting(name) {
     console.log(`Hola ${name}`);
   }
